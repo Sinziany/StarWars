76413939144de801b4584418b21fa2eb1a89b282
Sinziana Flueras <any.sinziana@gmail.com>
2020-01-26 19:27:14 +0200

    Updates

---
+++ precache-manifest.57876021761f1a08e60a5fb3a85fa696.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe7d9de3adec21d28814debd667bb8cc",
+    "url": "/StarWars/index.html"
+  },
+  {
+    "revision": "fc1c238d95a1c3777843",
+    "url": "/StarWars/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "9ab06d2750c2da09d0bb",
+    "url": "/StarWars/static/css/main.3c8093fa.chunk.css"
+  },
+  {
+    "revision": "fc1c238d95a1c3777843",
+    "url": "/StarWars/static/js/2.32b5a63d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/StarWars/static/js/2.32b5a63d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9ab06d2750c2da09d0bb",
+    "url": "/StarWars/static/js/main.3b04443b.chunk.js"
+  },
+  {
+    "revision": "99e52491242d93ef8f3a",
+    "url": "/StarWars/static/js/runtime-main.754980d5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/StarWars/static/media/SEGA.12a3db8b.woff"
+  }
+]);
